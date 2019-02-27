3ad14583fc853917571d15b170d29a8b34e0e44a
jinlizy117 <jinlizy117@sina.com>
2019-02-27 08:07:11 +0800

    社交系统： 基础微服务、招聘微服务、活动微服务、问答微服务、文章微服务、吐槽微服务、文章评论

---
+++ tensquare_recuit/src/main/java/com/tensquare/recuit/dao/EnterpriseDao.java
@@ -0,0 +1,17 @@
+package com.tensquare.recuit.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.tensquare.recuit.pojo.Enterprise;
+
+import java.util.List;
+
+/**
+ * 数据访问接口
+ * @author Administrator
+ *
+ */
+public interface EnterpriseDao extends JpaRepository<Enterprise,String>,JpaSpecificationExecutor<Enterprise>{
+	public List<Enterprise> findByIshot(String ishot);  //where ishot = ?
+}
